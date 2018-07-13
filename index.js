81164f4f843627490fae28b5e61f4da6b9fa9b20
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-07-13 20:42:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function findMinAndRemoveSorted(array) {
 
 function merge(array1, array2) {
   let sorted = [];
+<<<<<<< HEAD
   while (array1.length !== 0 && array2.length !== 0) {
     if (array1[0] < array2[0]) {
       sorted.push(findMinAndRemoveSorted(array1));
@@ -25,4 +26,18 @@ function mergeSort(array) {
     sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf));
   }
   return sorted;
+=======
+  let min;
+  while (array1 !== 0 && array2 !== 0) {
+    let firstHalfMin = findMinAndRemoveSorted(array1);
+    let secondHalfMin = findMinAndRemoveSorted(array2)
+    if (firstHalfMin > secondHalfMin) {
+      min = firstHalfMin; 
+    } else {
+      min = secondHalfMin
+    }
+    sorted.push(min);
+  }
+  return sorted.concat(array1).concat(array2);
+>>>>>>> 101afd3e019a1b0808bf934fd2229284f4f2f670
 }
